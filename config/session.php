1eb194025ebac33e556bd19ae5dd9be005add472
Erick Ccantillo <erick.ccantillo97@gmail.com>
2026-03-04 20:38:40 -0500

    fix: Update session lifetime to 18000 seconds for extended session duration

--- config/session.php
+++ config/session.php
@@ -32,7 +32,7 @@ return [
     |
     */
 
-    'lifetime' => (int) env('SESSION_LIFETIME', 120),
+    'lifetime' => (int)  18000,
 
     'expire_on_close' => env('SESSION_EXPIRE_ON_CLOSE', false),
 
